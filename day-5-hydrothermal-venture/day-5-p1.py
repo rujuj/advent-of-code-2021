02469827704f0f7168d6166aafd1a14c4dc28566
Ruju <rujykr@gmail.com>
2021-12-11 22:14:33 -0500

    just saving current work before i redo some of it

--- day-5-hydrothermal-venture/day-5-p1.py
+++ day-5-hydrothermal-venture/day-5-p1.py
@@ -22,9 +22,58 @@ for line in lines:
     if y2 > max_y:
         max_y = y2
 
-map = [["."] * max_x] * max_y
-print(map)
-print(len(map))
-print(len(map[0]))
+# initialize the map 
+map = [[0] * (max_y + 1) for _ in range(max_x + 1)]
+# print(map)
+# print(len(map))
+# print(len(map[0]))
+
+# for each set of coordinates, mark the vents with numbers (increment by 1)
+
+# REDO THIS SHIT LOL
+
+for vent in coordinates:
+    print(vent)
+    # if the 2 y-coordinates match, it's a horizontal line
+    if vent[0][0] == vent[1][0]:
+        y = vent[0][0]
+        if vent[0][0] > vent[1][0]:
+            x1 = vent[0][0]
+            x2 = vent[1][0]
+        else:
+            x1 = vent[1][0]
+            x2 = vent[0][0]
+        print(range(x1, x2+1))
+        for x in range(x1, x2 + 1):
+            if map[x][y] == None:
+                map[x][y] = 1
+            else:
+                map[x][y] += 1
+    # if the 2 x-coordinates match, it's a vertical line
+    elif vent[0][1] == vent[1][1]:
+        x = vent[0][1]
+        if vent[1][0] > vent[1][1]:
+            y1 = vent[1][0]
+            y2 = vent[1][1]
+        else:
+            y1 = vent[1][1]
+            y2 = vent[1][0]
+        print(range(y1, y2+1))
+        for y in range(y1, y2 + 1):
+            if map[x][y] == None:
+                map[x][y] = 1
+            else:
+                map[x][y] += 1
+
+dangerous = 0
+for row in map:
+    for spot in row:
+        if spot >= 2:
+            dangerous += 1
+    print(row)
+
+print(dangerous)
+
+
 
 # 
